609d063fa8719ce1e2c3b87ec3945595a4b1ec33
fuji <fhkt863@gmail.com>
2021-09-03 03:54:41 +0900

    first commit

---
+++ test/pages/index.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render, fireEvent } from '../testUtils'
+import { Home } from '../../pages/index'
+
+describe('Home page', () => {
+  it('renders a heading', () => {
+    const { getByRole } = render(<Home />)
+
+    const heading = getByRole('heading', {
+      name: /welcome to next\.js!/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('clicking button triggers alert', () => {
+    const { getByText } = render(<Home />, {})
+    window.alert = jest.fn()
+    fireEvent.click(getByText('Test Button'))
+    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
+  })
+})
